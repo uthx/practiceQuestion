a6328679623bb92ca1bd96fc07611fa46b1003a0
uthx <rdrahuldhiman.19@gmail.com>
2022-01-09 21:53:01 +0530

    added comments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Easy 1
+// Easy 1, add 5 nums
 const add = (...numRest) => numRest.reduce((a,b) => a+b,0);
 
-//Easy 2
+//Easy 2, check odd or even
 const checkIsEven = num => num % 2 === 0 ? "even" : "odd";
